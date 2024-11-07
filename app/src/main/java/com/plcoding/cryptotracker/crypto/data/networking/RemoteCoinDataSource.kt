219ef32be23e7c4f1304c3acc37aa5f869f0cc56
Mo R. Abed <37982720+MohammedRAbed@users.noreply.github.com>
2024-11-07 19:56:37 +0200

    fetch data from api

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -0,0 +1,26 @@
+package com.plcoding.cryptotracker.crypto.data.networking
+
+import com.plcoding.cryptotracker.core.data.networking.constructUrl
+import com.plcoding.cryptotracker.core.data.networking.safeCall
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+import com.plcoding.cryptotracker.core.domain.util.Result
+import com.plcoding.cryptotracker.core.domain.util.map
+import com.plcoding.cryptotracker.crypto.data.networking.dto.CoinListDto
+import com.plcoding.cryptotracker.crypto.data.toCoin
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+
+class RemoteCoinDataSource(
+    private val httpClient: HttpClient
+) : CoinDataSource {
+    override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
+        return safeCall<CoinListDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets")
+            )
+        }.map { response -> response.coinsList.map { it.toCoin() } }
+    }
+
+}
